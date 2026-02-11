5fff3f6b5faf532302ec0f51e5a9abd107df23f0
hassanalam014 <116816975+hassanalam014@users.noreply.github.com>
2026-02-11 21:51:07 +0000

    updated functions

---
+++ all_p_params.py
@@ -0,0 +1,37 @@
+#Sanchez-Lacombe parameters for the pure POLYMER.
+
+#======================================================
+#HASSAN ALAM Regressed Characteristic Parameters for Data1
+#======================================================
+# Data 1 Matching 1
+# Paper: Best 2012 PVT and Tg of PC Aravind 1-s2.0-S014294181100153X-main
+Pstar_1=477.201600      #+/- 6.50120144 (1.36%) (init = 60)
+Tstar_1=745.977589      #+/- 2.32835065 (0.31%) (init = 50)
+Rstar_1=1.28039144      #+/- 0.00124800 (0.10%) (init = 2)
+
+#======================================================
+#HASSAN ALAM Regressed Characteristic Parameters for Data2
+#======================================================
+# Data 2 Matching 2
+# Paper: Good 1997 PVT of PC (SICI)1097-4628(19971003)66_1_141__AID-APP17_3.0.CO;2-4
+Pstar_2=500.834279      #+/- 11.7676771 (2.35%) (init = 60)
+Tstar_2=751.670242      #+/- 4.73421735 (0.63%) (init = 50)
+Rstar_2=1.28175322      #+/- 0.00261206 (0.20%) (init = 2)
+
+#======================================================
+#HASSAN ALAM Regressed Characteristic Parameters for Data3
+#======================================================
+# Data 3 Overlap But Not Matching
+# Paper: Best 2016 PVT and Tg of PC Rudolph Rudolph2016_Article_WLFModelForThePressureDependen
+Pstar_3=378.30332       #+/- 11.5504425 (3.05%) (init = 60)
+Tstar_3=625.52729       #+/- 3.99028295 (0.64%) (init = 50)
+Rstar_3=1.33244470      #+/- 0.00373602 (0.28%) (init = 2)
+
+#======================================================
+#HASSAN ALAM Regressed Characteristic Parameters for Data4
+#======================================================
+# Data 4 Neither Matching Nor Overlap
+# Paper: Good 2003 PVT and Tg of PS and PC Kikuchi Thermal Conductivity
+Pstar_4=899.363789      #+/- 14.6137301 (1.62%) (init = 60)
+Tstar_4=659.224862      #+/- 2.94947589 (0.45%) (init = 50)
+Rstar_4=1.22050074      #+/- 0.00213321 (0.17%) (init = 2)
